66cd1a9ead0015527703b4a6d679edd9c1e25dff
pcyyura <cyp.yura@gmail.com>
2025-06-02 11:37:38 +0800

    feat: add inventory list to admin page

--- src/views/Admin/AdminInventory.vue
+++ src/views/Admin/AdminInventory.vue
@@ -1,35 +1,44 @@
 <script setup>
   import CommonTable from '@/components/CommonTable.vue'
   import { ref } from 'vue'
-  const inventoryTabs = ref([
-    { title: '全部', content: 'Tab 1 Content', value: 'all' },
-    { title: '上架中', content: 'Tab 1 Content', value: 'active' },
-    { title: '草稿', content: 'Tab 2 Content', value: 'draft' },
-    { title: '典藏', content: 'Tab 3 Content', value: 'archived' },
-  ])
+
+  const inventoryTabs = ref([{ title: '全部', value: 'all' }])
   const inventoryColumns = ref([
-    { field: 'name', header: '商品', style: 'width: 25%' },
-    { field: 'status', header: '狀態', style: 'width: 25%' },
-    { field: 'stock', header: '庫存數量', style: 'width: 25%' },
+    { field: 'img', header: '', style: 'width: 25%', sortable: false },
+    { field: 'name', header: '商品', style: 'width: 25%', sortable: true },
+    {
+      field: 'reserved',
+      header: '預留數量',
+      style: 'width: 25%',
+      sortable: true,
+    },
+    {
+      field: 'stockOnHand',
+      header: '現有庫存',
+      style: 'width: 25%',
+      sortable: true,
+    },
   ])
   const inventoryValue = ref([
     {
       img: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      status: 'Accessories',
-      stock: 24,
+      name: '路卡力歐',
+      reserved: 2,
+      stockOnHand: 24,
     },
   ])
 </script>
 
 <template>
-  <h2 class="mb-4 text-2xl">庫存</h2>
+  <div class="flex justify-between items-center mr-8 mb-4">
+    <h2 class="text-2xl">庫存</h2>
+  </div>
   <CommonTable
     :tabs="inventoryTabs"
     :columns="inventoryColumns"
     :value="inventoryValue"
-    :scrollable
-    :selectable
-    :scroll-height
+    scrollable
+    selectable
+    scroll-height="500px"
   />
 </template>
