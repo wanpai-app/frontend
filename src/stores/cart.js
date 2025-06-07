55894be8774708dae1fe545d511cfca142bc0619
hsiehyuhan <yuhan68108@gmail.com>
2025-06-08 00:38:15 +0800

    activate home page add-to-shopping-cart icon

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    items: [],
+    items: JSON.parse(localStorage.getItem('cart') || '[]'),
   }),
   getters: {
     totalQty: (s) => s.items.reduce((a, b) => a + b.qty, 0),
@@ -13,16 +13,23 @@ export const useCartStore = defineStore('cart', {
       const e = this.items.find((i) => i.id === product.id)
       if (e) e.qty++
       else this.items.push({ ...product, qty: 1 })
+      this.saveToLocalStorage()
     },
     updateQty(id, qty) {
       const i = this.items.find((x) => x.id === id)
       if (i) i.qty = qty < 1 ? 1 : qty
+      this.saveToLocalStorage()
     },
     remove(id) {
       this.items = this.items.filter((i) => i.id !== id)
+      this.saveToLocalStorage()
     },
     clear() {
       this.items = []
+      this.saveToLocalStorage()
+    },
+    saveToLocalStorage() {
+      localStorage.setItem('cart', JSON.stringify(this.items))
     },
   },
 })
