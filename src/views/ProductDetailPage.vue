fc04eb1a253b8954549633aaa4d19774b7e4c07f
Nan <austin.tctw@gmail.com>
2025-06-12 18:15:03 +0800

    fix/ProductDetailPage

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -2,10 +2,9 @@
   import { ref, onMounted, nextTick } from 'vue'
   import { useRoute } from 'vue-router'
   import axios from 'axios'
-
+  import Image from 'primevue/image'
   import Button from 'primevue/button'
   import InputNumber from 'primevue/inputnumber'
-  import Image from 'primevue/image'
 
   const route = useRoute()
   const product = ref({})
