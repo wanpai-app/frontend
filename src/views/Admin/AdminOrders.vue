00441c9b14e7720cfe520c5890ab1e38e3e51b77
pcyyura <cyp.yura@gmail.com>
2025-06-22 13:22:41 +0800

    feat: add admin fetchAllOrders api

--- src/views/Admin/AdminOrders.vue
+++ src/views/Admin/AdminOrders.vue
@@ -4,7 +4,11 @@
   import { onMounted } from 'vue'
   import { useRouter } from 'vue-router'
   import CommonTable from '@/components/CommonTable.vue'
+  import { useToast } from 'primevue/usetoast'
+  import Toast from 'primevue/toast'
   const router = useRouter()
+  const toast = useToast()
+
   const orderTabs = ref([
     { title: '全部', value: 'all' },
     { title: '已付款', value: 'paid' },
@@ -16,36 +20,46 @@
     {
       field: 'orderNumber',
       header: '訂單編號',
-      style: 'width: 12.5%',
+      style: 'width: 20%',
       sortable: true,
     },
     { field: 'date', header: '日期', style: 'width: 12.5%', sortable: true },
     {
       field: 'customer',
       header: '顧客',
-      style: 'width: 12.5%',
+      style: 'width: 20%',
       sortable: false,
-    },
-    {
-      field: 'payment',
-      header: '付款狀態',
-      style: 'width: 12.5%',
-      sortable: true,
     },
     {
       field: 'status',
       header: '訂單狀態',
-      style: 'width: 12.5%',
+      style: 'width: 20%',
       sortable: true,
+      format: (val) =>
+        ({
+          paid: '已付款',
+          shipped: '已出貨',
+          completed: '已完成',
+          cancelled: '已取消',
+        })[val] || '未知',
     },
-    { field: 'total', header: '總金額', style: 'width: 12.5%', sortable: true },
-    {
-      field: 'quantity',
-      header: '數量',
-      style: 'width: 12.5%',
-      sortable: true,
-    },
+    { field: 'total', header: '總金額', style: 'width: 20%', sortable: true },
   ])
+  const currentStatus = ref('all')
+  const handleTabChange = async (newStatus) => {
+    currentStatus.value = newStatus
+    try {
+      const res = await fetchAllOrders(newStatus)
+      orderValue.value = res
+    } catch {
+      toast.add({
+        severity: 'warn',
+        summary: '訂單暫時無法載入',
+        detail: '請稍後再試',
+        life: 3000,
+      })
+    }
+  }
 
   const orderValue = ref([])
   onMounted(async () => {
@@ -55,21 +69,23 @@
 </script>
 
 <template>
+  <Toast />
   <div class="flex justify-between items-center mr-8 mb-4">
     <h2 class="text-2xl">訂單</h2>
-    <div class="card flex justify-center">
+    <!-- <div class="card flex justify-center">
       <button
         class="text-md text-white px-3 py-2 border-surface-200 rounded-md bg-primary cursor-pointer"
       >
         新增訂單
       </button>
-    </div>
+    </div> -->
   </div>
   <div class="mr-8">
     <CommonTable
       :tabs="orderTabs"
       :columns="orderColumns"
       :value="orderValue"
+      @tab-change="handleTabChange"
       scrollable
       selectable
       scroll-height="500px"
