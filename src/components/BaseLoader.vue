a6f875bf4f5e3afb7e6541575f228f13840ea9be
Nan <austin.tctw@gmail.com>
2025-06-27 15:59:59 +0800

    fix/conflict

---
+++ src/components/BaseLoader.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+  isLoading: Boolean,
+  hasLoadedOnce: Boolean,
+})
+
+</script>
+
+<template>
+  <div v-if="!hasLoadedOnce || isLoading" class="text-center text-gray-500 py-10">
+    <i class="pi pi-spin pi-spinner text-2xl mb-2"></i>
+    <slot>
+      <p>載入中...</p>
+    </slot>
+  </div>
+</template>
