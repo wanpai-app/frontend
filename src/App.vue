df93f7783fda66fdeb07949c52f15ca028e4a2bd
Nan <austin.tctw@gmail.com>
2025-07-01 11:21:44 +0800

    fixgap

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
   import { useRoute } from 'vue-router'
   import Navbar from '@/components/layout/NavBar.vue'
   import Footer from '@/components/layout/Footer.vue'
-  import { useToast } from 'primevue/usetoast'
   import Toast from 'primevue/toast'
   import FloatingBubble from '@/components/FloatingBubble.vue'
   import BubbleBackground from '@/components/BubbleBackground.vue'
+  import { useToast } from 'primevue/usetoast'
   import { setGlobalToast } from '@/utils/toast'
 
   const toast = useToast()
@@ -19,7 +19,6 @@
   const handleScroll = () => {
     const isHome = route.path === '/'
     const isSearching = !!route.query.keyword
-
     if (
       isHome &&
       !isSearching &&
@@ -33,21 +32,22 @@
   onMounted(() => {
     window.addEventListener('scroll', handleScroll)
   })
-
   onUnmounted(() => {
     window.removeEventListener('scroll', handleScroll)
   })
 </script>
 
 <template>
-  <Toast />
-  <BubbleBackground />
-  <FloatingBubble ref="bubbleRef" />
-  <Navbar v-if="!isAdmin" />
+  <div class="min-h-screen flex flex-col relative overflow-x-hidden">
+    <Toast />
+    <BubbleBackground class="absolute inset-0 -z-10" />
+    <FloatingBubble ref="bubbleRef" />
+    <Navbar v-if="!isAdmin" />
 
-  <main :class="!isAdmin ? 'mt-6 min-h-screen' : ''">
-    <RouterView />
-  </main>
+    <main :class="!isAdmin ? 'flex-1' : ''">
+      <RouterView />
+    </main>
 
-  <Footer v-if="!isAdmin" />
+    <Footer v-if="!isAdmin" />
+  </div>
 </template>
