ef4f608a53500728f4662e1ae7338dd7646ce33e
Rae lin <llllyuxx@gmail.com>
2025-07-02 16:35:46 +0800

    fix:adjust the style

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
     extend: {
       keyframes: {
         floatX: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(12px)' },
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
         },
         fadeIn: {
           '0%': { opacity: 0 },
@@ -22,6 +22,7 @@ export default {
       animation: {
         floatX: 'floatX 4s ease-in-out infinite',
         fadeIn: 'fadeIn 1s ease-in-out forwards',
+        marquee: 'marquee 20s linear infinite',
       },
     },
   },
